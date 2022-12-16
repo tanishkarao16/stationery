87a19c6e938b970658be8172eedd9084c02d39b4
tanishkarao16 <74966490+tanishkarao16@users.noreply.github.com>
2022-12-16 09:08:28 +0530

    fix

--- shop.php
+++ shop.php
@@ -38,10 +38,8 @@ if(isset($_POST['add_to_cart'])){
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>shop</title>
 
-   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-   <!-- custom css file link  -->
    <link rel="stylesheet" href="style1.css">
 
 </head>
